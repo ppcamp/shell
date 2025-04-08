de1da15df5ad3d1e4448e531223c1487af4906cf
Pedro Santos <p.augustocampos@gmail.com>
2025-04-08 19:46:47 -0300

    feat: add scripts

--- tools/scripts/install_this_repo.zsh
+++ tools/scripts/install_this_repo.zsh
@@ -7,17 +7,18 @@
 # curl -sL https://raw.githubusercontent.com/ppcamp/shell/refs/heads/main/tools/scripts/install_this_repo.zsh | INSTALL_DIR=$HOME/.config/shell zsh -
 
 INSTALL_DIR=${INSTALL_DIR:-$HOME/.config/shell}
-echo $INSTALL_DIR
-# ZSHRC_INIT_FILE=${INSTALL_DIR}/init.zsh
+echo "Installing it at $INSTALL_DIR"
 
-# git clone --recursive https://github.com/ppcamp/shell $(INSTALL_DIR)
-# echo "Installed shell at $INSTALL_DIR"
+ZSHRC_INIT_FILE=${INSTALL_DIR}/init.zsh
 
-# echo "Setup zshrc"
-# echo "source $ZSHRC_INIT_FILE" >>$HOME/.zshrc
+git clone --recursive https://github.com/ppcamp/shell ${INSTALL_DIR}
+echo "Installed shell at $INSTALL_DIR"
 
-# echo "Installing xcp/xpb scripts (required)"
-# pushd $(pwd) >/dev/null
-# cd $INSTALL_DIR
-# make install-xcp
-# popd >/dev/null
+echo "Setup zshrc"
+echo "source $ZSHRC_INIT_FILE" >>$HOME/.zshrc
+
+echo "Installing xcp/xpb scripts (required)"
+pushd $(pwd) >/dev/null
+cd $INSTALL_DIR
+make install-xcp
+popd >/dev/null
