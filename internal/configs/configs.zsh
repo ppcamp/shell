049151816eb04051822bcd6d2f169897603d175e
Pedro Santos <p.augustocampos@gmail.com>
2025-04-15 20:47:49 -0300

    feat: add new option to zsh history opt

--- internal/configs/configs.zsh
+++ internal/configs/configs.zsh
@@ -16,7 +16,7 @@ setopt HIST_IGNORE_ALL_DUPS        # Delete an old recorded event if a new event
 setopt HIST_IGNORE_SPACE           # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS           # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY                 # Do not execute immediately upon history expansion.
-#setopt INC_APPEND_HISTORY         # Write to the history file immediately, not when the shell exits.
+setopt INC_APPEND_HISTORY          # Write to the history file immediately, not when the shell exits.
 #setopt EXTENDED_HISTORY           # Write the history file in the ':start:elapsed;command' format.
 #setopt APPEND_HISTORY             # append to history file
 #setopt HIST_NO_STORE              # Don't store history commands
