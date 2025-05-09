bd7d11ef0d1f849e354798956df428177b8083f8
Pedro Santos <pedro.santos@emailprovider.com>
2025-05-09 13:41:20 -0300

    fix: snippets pull

--- internal/configs/snippets.zsh
+++ internal/configs/snippets.zsh
@@ -1,9 +1,12 @@
 # vim: ts=2 sts=2 sw=2 et:
 
 # Gist dotfiles
-local url="https://gist.github.com/ppcamp/a5d7828f3430226b2668b2f29bc38485/raw"
+local url="https://gist.githubusercontent.com/ppcamp/a5d7828f3430226b2668b2f29bc38485/raw"
 for file in '.gitconfig' '.git-template'; do
-  zinit ice atclone"[ -f ~/.$file ] && mv ~/.$file{,.bk} || ln -sf \${PWD}/$file ~/.${file}" nocompile
+  zinit ice \
+    atclone"[ -f ~/$file ] && mv ~/$file{,.bk}; cp -f \${PWD}/$file ~/$file" \
+    atpull"[ -f ~/$file ] && mv ~/$file{,.bk2}; cp -f \${PWD}/$file ~/$file" \
+    nocompile as'null' lucid
   zinit snippet "$url/$file"
 done
 
@@ -20,3 +23,11 @@ for file in $toclone; do
   zinit ice wait"0a" lucid
   zinit snippet "$url/$file"
 done
+
+# extra file (related to fd-find)
+file='.fdignore'
+zinit ice \
+  atclone"[ -f ~/$file ] && mv ~/$file{,.bk}; cp -f \${PWD}/$file ~/$file" \
+  atpull"[ -f ~/$file ] && mv ~/$file{,.bk2}; cp -f \${PWD}/$file ~/$file" \
+  nocompile as'null' lucid
+zinit snippet "$url/$file"
